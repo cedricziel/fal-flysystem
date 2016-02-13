7c28d9b86813fd7cd2e163824cd43a0d9d900868
Cedric Ziel <cedric@cedric-ziel.com>
2016-02-13 21:53:38 +0100

    Add author info

--- Classes/Fal/FlysystemDriver.php
+++ Classes/Fal/FlysystemDriver.php
@@ -2,11 +2,36 @@
 
 namespace CedricZiel\FalFlysystem\Fal;
 
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2016 Cedric Ziel <cedric@cedric-ziel.com>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\AdapterInterface;
 use League\Flysystem\Config;
 use League\Flysystem\FilesystemInterface;
 use TYPO3\CMS\Core\Resource\Driver\AbstractHierarchicalFilesystemDriver;
+use TYPO3\CMS\Core\Resource\Exception\FileDoesNotExistException;
 use TYPO3\CMS\Core\Resource\Exception\FileOperationErrorException;
 use TYPO3\CMS\Core\Resource\Exception\FolderDoesNotExistException;
 use TYPO3\CMS\Core\Resource\Exception\InvalidFileNameException;
@@ -542,12 +567,13 @@ abstract class FlysystemDriver extends AbstractHierarchicalFilesystemDriver
      * @param array $propertiesToExtract Array of properties which are be extracted
      *                                   If empty all will be extracted
      * @return array
+     * @throws FileDoesNotExistException
      */
     public function getFileInfoByIdentifier($fileIdentifier, array $propertiesToExtract = [])
     {
         $relativeDriverPath = ltrim($fileIdentifier, '/');
         if (!$this->filesystem->has($relativeDriverPath) || !$this->filesystem->get($relativeDriverPath)->isFile()) {
-            throw new \InvalidArgumentException('File ' . $fileIdentifier . ' does not exist.', 1314516809);
+            throw new FileDoesNotExistException('File ' . $fileIdentifier . ' does not exist.', 1314516809);
         }
         $dirPath = PathUtility::dirname($fileIdentifier);
         $dirPath = $this->canonicalizeAndCheckFolderIdentifier($dirPath);
--- Tests/Unit/Fal/VfsDriverTest.php
+++ Tests/Unit/Fal/VfsDriverTest.php
@@ -2,6 +2,30 @@
 
 namespace CedricZiel\FalFlysystem\Tests\Unit\Fal;
 
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2016 Cedric Ziel <cedric@cedric-ziel.com>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
 use CedricZiel\FalFlysystem\Fal\VfsDriver;
 use PHPUnit_Framework_TestCase;
 use TYPO3\CMS\Core\Utility\PathUtility;
@@ -227,7 +251,7 @@ class VfsDriverTest extends PHPUnit_Framework_TestCase
         $driver = $this->getInitializedDriver();
 
         $driver->getFilesystem()->createDir('test');
-        $folderInfo = $driver->getFolderInfoByIdentifier('/test');
+        $folderInfo = $driver->getFolderInfoByIdentifier('/test/');
         $expectedFolderInfo = [
             'name' => 'test',
             'identifier' => '/test/',
